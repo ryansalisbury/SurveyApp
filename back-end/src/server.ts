a38a98117c3e3abdcb51b625bee8fedb2575612e
Ryan Salisbury <ryansalisbury10@icloud.com>
2024-03-21 09:25:14 +0000

    fastify backend setup, mongoDB connected

---
+++ back-end/src/server.ts
@@ -0,0 +1,37 @@
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import fastifyMongo from "@fastify/mongodb"; // Adjusted import
+import dotenv from "dotenv";
+dotenv.config();
+const mongoDbUri = process.env.MONGODB_URI;
+console.log(`MongoDB URI: ${mongoDbUri}`);
+
+const buildServer = () => {
+  const server = fastify({ logger: true });
+
+  server.register(fastifyCors); // Adjust CORS settings as needed
+  server.register(fastifyMongo, {
+    url: mongoDbUri,
+  });
+
+  // Your route definitions here
+  server.get("/ping", async (request, reply) => {
+    return { pong: "it works!" };
+  });
+
+  return server;
+};
+
+const startServer = async () => {
+  const server = buildServer();
+
+  try {
+    await server.listen({ port: 3001, host: "0.0.0.0" });
+    console.log("Server listening on port http://localhost:3001");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
